eef38a585bf5af4d97ca78273c91c3207e04fdd9
thanh090902 <thanhthaiit92@gmail.com>
2025-07-29 17:21:14 +0700

    alert file commit

---
+++ tests/alert.test.ts
@@ -0,0 +1,7 @@
+import {test, expect} from '@playwright/test';  
+
+test('Alert Test', async ({ page }) => {
+  await page.goto("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo");
+
+  
+});
